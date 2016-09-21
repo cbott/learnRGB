a934b1a826d836862e549b6ab4001ee06b37630a
cbott <cbott6@gmail.com>
2016-09-21 10:17:33 -0400

    Moved code into a class

--- colorlearn.py
+++ colorlearn.py
@@ -2,69 +2,76 @@ from Tkinter import *
 import random
 import tkFont
 
-master = Tk()
+class Application(Frame):
+    def __init__(self, master):
+        #canvas / colored rectangle dimensions
+        self.width = 1000
+        self.height = 500
+        #Actual color of the displayed rectangle (r,g,b)
+        self.current_color = (0,0,0)
 
-#just a big font so things are bigger
-big_font = tkFont.Font(root=master,family='Helvetica',size=24)
+        #just a big font so things are bigger
+        self.big_font = tkFont.Font(root=master,family='Helvetica',size=24)
 
-#Actual color of the displayed rectangle
-current_red = 0
-current_green = 0
-current_blue = 0
+        Frame.__init__(self, master)
+        self.grid()
+        self.generate()
+        self.next_color()
 
-def next_color(change_focus=0):
-	global current_red, current_green, current_blue
-	current_red = random.randint(0,255)
-	current_green = random.randint(0,255)
-	current_blue = random.randint(0,255)
-	w.create_rectangle(0,0,1000,500, fill='#%02x%02x%02x' % (current_red, current_green, current_blue))
-	if change_focus:
-		r_in.focus()
+    def next_color(self):
+        """Change the color currently displayed"""
+        self.current_color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+        self.w.create_rectangle(0,0,1000,500, fill='#%02x%02x%02x' % self.current_color)
 
-def submit():
-	"""Display the results: comparing user-entered values to actual color values"""
-	global current_red, current_green, current_blue
+    def submit(self):
+        """Display the results: comparing user-entered values to actual color values"""
 
-	r = int(r_in.get())
-	g = int(g_in.get())
-	b = int(b_in.get())
+        r = int(self.r_in.get())
+        g = int(self.g_in.get())
+        b = int(self.b_in.get())
 
-	out.delete(1.0, END)
-	out.insert(END, "Actual Red:%i\n"%current_red)
-	out.insert(END, "Actual Green:%i\n"%current_green)
-	out.insert(END, "Actual Blue:%i\n"%current_blue)
+        self.out.delete(1.0, END)
+        self.out.insert(END, "Actual Red:%i\n"%self.current_color[0])
+        self.out.insert(END, "Actual Green:%i\n"%self.current_color[1])
+        self.out.insert(END, "Actual Blue:%i\n"%self.current_color[2])
 
-	score = int(((765 - abs(current_red - r) - abs(current_green - g) - abs(current_blue - b))/765.0)**2 * 100)
-	out.insert(END, "Your Score:%i"%score)
+        diffsum = abs(self.current_color[0] - r) + abs(self.current_color[1] - g) + abs(self.current_color[2] - b)
+        score = int(((765 - diffsum)/765.0)**2 * 100)
+        self.out.insert(END, "Your Score:%i"%score)
+            
+    def generate(self):
+        """Draw fields on the window"""
 
-#Canvas used to display the color rectangle
-w = Canvas(master, width=1000, height=500)
-w.grid(row=0, column=0, columnspan=4)
+        #Canvas used to display the color rectangle
+        self.w = Canvas(self, width=1000, height=500)
+        self.w.grid(row=0, column=0, columnspan=4)
 
-Label(master, font=big_font, text="Red:").grid(row=1, column=0)
-r_in = Entry(master, font=big_font)
-r_in.grid(row=1, column=1)
+        Label(self, font=self.big_font, text="Red:").grid(row=1, column=0)
+        self.r_in = Entry(self, font=self.big_font)
+        self.r_in.grid(row=1, column=1)
 
-Label(master, font=big_font, text="Green:").grid(row=2, column=0)
-g_in = Entry(master, font=big_font)
-g_in.grid(row=2,column=1)
+        Label(self, font=self.big_font, text="Green:").grid(row=2, column=0)
+        self.g_in = Entry(self, font=self.big_font)
+        self.g_in.grid(row=2,column=1)
 
-Label(master, font=big_font, text="Blue:").grid(row=3, column=0)
-b_in = Entry(master, font=big_font)
-b_in.grid(row=3, column=1)
+        Label(self, font=self.big_font, text="Blue:").grid(row=3, column=0)
+        self.b_in = Entry(self, font=self.big_font)
+        self.b_in.grid(row=3, column=1)
 
-btn_submit = Button(master, font=big_font, text="Submit!", command=submit)
-btn_submit.grid(row=4, column=1)
-btn_submit.bind('<Return>', lambda x:submit())
+        self.btn_submit = Button(self, font=self.big_font, text="Submit!", command=self.submit)
+        self.btn_submit.grid(row=4, column=1)
+        self.btn_submit.bind('<Return>', lambda x:self.submit())
 
-btn_color = Button(master, font=big_font, text="Next Color", command=next_color)
-btn_color.grid(row=4, column=0)
-#Automatically select the first color input if user hits enter while "next color" is active
-btn_color.bind('<Return>', lambda x:next_color(change_focus=1))
+        self.btn_color = Button(self, font=self.big_font, text="Next Color", command=self.next_color)
+        self.btn_color.grid(row=4, column=0)
+        #Automatically select the first color input if user hits enter while "next color" is active
+        self.btn_color.bind('<Return>', lambda x:[self.next_color(), self.r_in.focus()])
 
-#The text output section - display scores and results
-out = Text(master, width=50, height=4, font=big_font)
-out.grid(row=5, column=0, columnspan=2)
+        #The text output section - display scores and results
+        self.out = Text(self, width=50, height=4, font=self.big_font)
+        self.out.grid(row=5, column=0, columnspan=2)
 
-next_color()
-mainloop()
+root = Tk()
+root.title("Learn RGB")
+app = Application(root)
+root.mainloop()
