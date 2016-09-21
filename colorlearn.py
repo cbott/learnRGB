ba9ad433ce0381ed3463c4c40e275cf0446eb160
cbott <cbott6@gmail.com>
2016-09-20 23:47:38 -0400

    increase size of elements and fix tabbing through fields

--- colorlearn.py
+++ colorlearn.py
@@ -3,20 +3,26 @@ import random
 import tkFont
 
 master = Tk()
+
+#just a big font so things are bigger
 helv36 = tkFont.Font(root=master,family='Helvetica',size=24)
 
+#Actual color of the displayed rectangle
 current_red = 0
 current_green = 0
 current_blue = 0
 
-def next_color():
+def next_color(change_focus=0):
 	global current_red, current_green, current_blue
 	current_red = random.randint(0,255)
 	current_green = random.randint(0,255)
 	current_blue = random.randint(0,255)
 	w.create_rectangle(0,0,1000,500, fill='#%02x%02x%02x' % (current_red, current_green, current_blue))
+	if change_focus:
+		r_in.focus()
 
 def submit():
+	"""Display the results: comparing user-entered values to actual color values"""
 	global current_red, current_green, current_blue
 
 	r = int(r_in.get())
@@ -31,27 +37,32 @@ def submit():
 	score = int(((765 - abs(current_red - r) - abs(current_green - g) - abs(current_blue - b))/765.0)**2 * 100)
 	out.insert(END, "Your Score:%i"%score)
 
+#Canvas used to display the color rectangle
 w = Canvas(master, width=1000, height=500)
 w.grid(row=0, column=0, columnspan=4)
 
-Label(master, text="Red:").grid(row=1, column=0)
-r_in = Entry(master)
+Label(master, font=helv36, text="Red:").grid(row=1, column=0)
+r_in = Entry(master, font=helv36)
 r_in.grid(row=1, column=1)
 
-Label(master, text="Green:").grid(row=2, column=0)
-g_in = Entry(master)
+Label(master, font=helv36, text="Green:").grid(row=2, column=0)
+g_in = Entry(master, font=helv36)
 g_in.grid(row=2,column=1)
 
-Label(master, text="Blue:").grid(row=3, column=0)
-b_in = Entry(master)
+Label(master, font=helv36, text="Blue:").grid(row=3, column=0)
+b_in = Entry(master, font=helv36)
 b_in.grid(row=3, column=1)
 
-btn = Button(master, text="Submit!", command=submit)
-btn.grid(row=4, column=1)
+btn_submit = Button(master, font=helv36, text="Submit!", command=submit)
+btn_submit.grid(row=4, column=1)
+btn_submit.bind('<Return>', lambda x:submit())
 
-btn = Button(master, text="Next Color", command=next_color)
-btn.grid(row=4, column=0)
+btn_color = Button(master, font=helv36, text="Next Color", command=next_color)
+btn_color.grid(row=4, column=0)
+#Automatically select the first color input if user hits enter while "next color" is active
+btn_color.bind('<Return>', lambda x:next_color(change_focus=1))
 
+#The text output section - display scores and results
 out = Text(master, width=50, height=4, font=helv36)
 out.grid(row=5, column=0, columnspan=2)
 
