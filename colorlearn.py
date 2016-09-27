324a711e6d63ffe8f9af2709baaad9cbac4f91b8
cbott <cbott6@gmail.com>
2016-09-27 17:30:23 -0400

    working canvas scaling keeping other elements centered

--- colorlearn.py
+++ colorlearn.py
@@ -27,11 +27,16 @@ class Application(Frame):
         self.big_font = tkFont.Font(root=master, family='Helvetica', size=20)
 
         Frame.__init__(self, master)
-        self.grid()
+        self.grid(sticky=(E, W, S, N))
+
+        master.columnconfigure(0, weight=1)
+        master.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
         self.generate()
         self.next_color()
 
-        master.bind("<Configure>", self.resize)
 
     def next_color(self):
         """Change the color currently displayed"""
@@ -58,51 +63,48 @@ class Application(Frame):
         #Show the user what their answer was
         self.answer_canvas.create_rectangle(1, 20, self.ans_width-1, self.ans_height-1, fill=to_hex((r, g, b)))
 
-    def resize(self, event):
-        self.width = event.width
-        self.color_canvas.config(width=self.width, height=self.height)
-
     def generate(self):
         """Draw fields on the window"""
 
         #Canvas used to display the color rectangle
-        self.color_canvas = Canvas(self, width=self.width, height=self.height)
-        self.color_canvas.grid(row=0, column=0, columnspan=4)
+        self.color_canvas = Canvas(self)#, width=self.width, height=self.height)
+        self.color_canvas.grid(row=0, column=0, columnspan=3, sticky=(E, W, S, N))
 
+        self.content = Frame(self)
+        self.content.grid(sticky=(N))
 
-        Label(self, font=self.big_font, text="Red:").grid(row=1, column=0)
-        self.r_in = Entry(self, font=self.big_font)
+        Label(self.content, font=self.big_font, text="Red:").grid(row=1, column=0)
+        self.r_in = Entry(self.content, font=self.big_font)
         self.r_in.grid(row=1, column=1)
 
-        Label(self, font=self.big_font, text="Green:").grid(row=2, column=0)
-        self.g_in = Entry(self, font=self.big_font)
+        Label(self.content, font=self.big_font, text="Green:").grid(row=2, column=0)
+        self.g_in = Entry(self.content, font=self.big_font)
         self.g_in.grid(row=2, column=1)
 
-        Label(self, font=self.big_font, text="Blue:").grid(row=3, column=0)
-        self.b_in = Entry(self, font=self.big_font)
+        Label(self.content, font=self.big_font, text="Blue:").grid(row=3, column=0)
+        self.b_in = Entry(self.content, font=self.big_font)
         self.b_in.grid(row=3, column=1)
 
-        self.btn_submit = Button(self, font=self.big_font, text="Submit!", command=self.submit)
+        self.btn_submit = Button(self.content, font=self.big_font, text="Submit!", command=self.submit)
         self.btn_submit.grid(row=4, column=1)
         self.btn_submit.bind('<Return>', lambda x:self.submit())
 
-        self.btn_color = Button(self, font=self.big_font, text="Next Color", command=self.next_color)
+        self.btn_color = Button(self.content, font=self.big_font, text="Next Color", command=self.next_color)
         self.btn_color.grid(row=4, column=0)
         #Automatically select the first color input if user hits enter while "next color" is active
         self.btn_color.bind('<Return>', lambda x:[self.next_color(), self.r_in.focus()])
 
         #The text output section - display scores and results
-        self.out = Text(self, width=40, height=4, font=self.big_font)
+        self.out = Text(self.content, width=40, height=4, font=self.big_font)
         self.out.grid(row=5, column=0, columnspan=2)
 
         #Field to show the color that the user entered
-        self.answer_canvas = Canvas(self, width=self.ans_width, height=self.ans_height)
+        self.answer_canvas = Canvas(self.content, width=self.ans_width, height=self.ans_height)
         self.answer_canvas.create_rectangle(1, 1, self.ans_width, self.ans_height)
         self.answer_canvas.create_text(self.ans_width/2, 1, text="Your Answer:", anchor=N)
         self.answer_canvas.grid(row=5, column=2)
 
 root = Tk()
 root.title("Learn RGB")
-#root.resizable(0, 0)
 app = Application(root)
 root.mainloop()
