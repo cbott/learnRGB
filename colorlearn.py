9e0b85ca89e76a0772799f3f227698764be13079
cbott <cbott6@gmail.com>
2016-09-27 14:30:36 -0400

    attempt to call function on window resize

--- colorlearn.py
+++ colorlearn.py
@@ -3,6 +3,7 @@
 from Tkinter import *
 import random
 import tkFont
+import time
 
 def to_hex(rgb):
     """Change an (r, g, b) tuple to a tkinter-compatible hex string"""
@@ -26,14 +27,17 @@ class Application(Frame):
         self.big_font = tkFont.Font(root=master, family='Helvetica', size=20)
 
         Frame.__init__(self, master)
-        self.pack()
+        self.grid()
         self.generate()
         self.next_color()
+
+        master.bind("<Configure>", self.resize)
 
     def next_color(self):
         """Change the color currently displayed"""
         self.current_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        self.color_canvas.create_rectangle(0, 0, self.width, self.height, fill=to_hex(self.current_color))
+        self.color_canvas.delete(ALL)
+        self.color_canvas.config(bg = to_hex(self.current_color))
 
     def submit(self):
         """Display the results: comparing user-entered values to actual color values"""
@@ -54,51 +58,51 @@ class Application(Frame):
         #Show the user what their answer was
         self.answer_canvas.create_rectangle(1, 20, self.ans_width-1, self.ans_height-1, fill=to_hex((r, g, b)))
 
+    def resize(self, event):
+        self.width = event.width
+        self.color_canvas.config(width=self.width, height=self.height)
 
     def generate(self):
         """Draw fields on the window"""
 
         #Canvas used to display the color rectangle
         self.color_canvas = Canvas(self, width=self.width, height=self.height)
-        self.color_canvas.pack(fill=BOTH, expand=YES)
+        self.color_canvas.grid(row=0, column=0, columnspan=4)
 
-        self.left_frame = Frame(self)
-        self.left_frame.pack(side=LEFT)
-        self.right_frame = Frame(self)
-        self.right_frame.pack(side=RIGHT)
 
-        Label(self.left_frame, font=self.big_font, text="Red:").pack(anchor=E)
-        self.r_in = Entry(self.right_frame, font=self.big_font)
-        self.r_in.pack(anchor=W)
+        Label(self, font=self.big_font, text="Red:").grid(row=1, column=0)
+        self.r_in = Entry(self, font=self.big_font)
+        self.r_in.grid(row=1, column=1)
 
-        Label(self.left_frame, font=self.big_font, text="Green:").pack(anchor=E)
-        self.g_in = Entry(self.right_frame, font=self.big_font)
-        self.g_in.pack(anchor=W)
+        Label(self, font=self.big_font, text="Green:").grid(row=2, column=0)
+        self.g_in = Entry(self, font=self.big_font)
+        self.g_in.grid(row=2, column=1)
 
-        Label(self.left_frame, font=self.big_font, text="Blue:").pack(anchor=E)
-        self.b_in = Entry(self.right_frame, font=self.big_font)
-        self.b_in.pack(anchor=W)
+        Label(self, font=self.big_font, text="Blue:").grid(row=3, column=0)
+        self.b_in = Entry(self, font=self.big_font)
+        self.b_in.grid(row=3, column=1)
 
-        self.btn_submit = Button(self.right_frame, font=self.big_font, text="Submit!", command=self.submit)
-        self.btn_submit.pack()
+        self.btn_submit = Button(self, font=self.big_font, text="Submit!", command=self.submit)
+        self.btn_submit.grid(row=4, column=1)
         self.btn_submit.bind('<Return>', lambda x:self.submit())
 
-        self.btn_color = Button(self.left_frame, font=self.big_font, text="Next Color", command=self.next_color)
-        self.btn_color.pack()
+        self.btn_color = Button(self, font=self.big_font, text="Next Color", command=self.next_color)
+        self.btn_color.grid(row=4, column=0)
         #Automatically select the first color input if user hits enter while "next color" is active
         self.btn_color.bind('<Return>', lambda x:[self.next_color(), self.r_in.focus()])
 
         #The text output section - display scores and results
-        self.out = Text(self.left_frame, width=20, height=4, font=self.big_font)
-        self.out.pack()
+        self.out = Text(self, width=40, height=4, font=self.big_font)
+        self.out.grid(row=5, column=0, columnspan=2)
 
         #Field to show the color that the user entered
-        self.answer_canvas = Canvas(self.right_frame, width=self.ans_width, height=self.ans_height)
+        self.answer_canvas = Canvas(self, width=self.ans_width, height=self.ans_height)
         self.answer_canvas.create_rectangle(1, 1, self.ans_width, self.ans_height)
         self.answer_canvas.create_text(self.ans_width/2, 1, text="Your Answer:", anchor=N)
-        self.answer_canvas.pack()
+        self.answer_canvas.grid(row=5, column=2)
 
 root = Tk()
 root.title("Learn RGB")
+#root.resizable(0, 0)
 app = Application(root)
 root.mainloop()
